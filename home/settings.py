ffd9878a614404052470a609c8ed56aa62544b35
ahmed21-rg <rajpootahmed223@gmail.com>
2026-01-30 15:10:36 +0500

    Modify MEDIA_ROOT based on environment
    Updated MEDIA_ROOT configuration for Railway environment.

--- home/settings.py
+++ home/settings.py
@@ -160,15 +160,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+
+# Use an absolute path for Railway
+if os.environ.get("RAILWAY_ENVIRONMENT"):
+    MEDIA_ROOT = "/app/media"
+else:
+    MEDIA_ROOT = BASE_DIR / "media"
+
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
