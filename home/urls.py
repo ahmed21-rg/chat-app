fec81bf3bf37811561ef92db962f9e3e321b4458
ahmed21-rg <rajpootahmed223@gmail.com>
2026-02-02 10:13:10 +0500

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path( "", views.login_page, name="login_page"),
 
     path('api/', include('main.urls'))
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
